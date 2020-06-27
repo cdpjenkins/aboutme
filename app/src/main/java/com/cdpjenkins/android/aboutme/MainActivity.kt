ca10a9e649e62e68369a44e4b2155aeb0ccf4719
Chris Jenkins <cdpjenkins@gmail.com>
2020-06-27 09:27:37 +0100

    Lesson 2.13: Add onClickListener

--- app/src/main/java/com/cdpjenkins/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/cdpjenkins/android/aboutme/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.cdpjenkins.android.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener(this::addNickname)
+    }
+
+    fun addNickname(doneButton: View) {
+        val nicknameEdit = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = nicknameEdit.text
+        nicknameEdit.visibility = View.GONE
+        doneButton.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        hideKeyboard(doneButton)
+    }
+
+    private fun hideKeyboard(view: View) {
+        val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
